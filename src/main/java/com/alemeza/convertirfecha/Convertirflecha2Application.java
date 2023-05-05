f6cfd5d12c56fa6a082c2b92ca7b70d562f47f3a
Jandrocks <32751139+Jandrocks@users.noreply.github.com>
2023-05-05 09:54:26 -0400

    first commit

---
+++ src/main/java/com/alemeza/convertirfecha/Convertirflecha2Application.java
@@ -0,0 +1,13 @@
+package com.alemeza.convertirfecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Convertirflecha2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Convertirflecha2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alemeza/convertirfecha/Convertirflecha2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alemeza.convertirfecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Convertirflecha2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
